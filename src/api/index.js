ce13e5a4eba06350addf36f5e9d8fd387b1c4b63
Barron Roth <barronlroth@gmail.com>
2016-11-01 20:01:00 -0400

    starting social account tutorial

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var todos = require('../../mock/todos.json');
+
+
+// router - automatically change routes
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+	res.json({todos: todos});
+});
+
+// TODO: Add POST route to create new entries
+
+// TODO: Add PUT route to update existing entries
+
+// TODO: Add DELETE route to delete entries
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 // route - entire public folder is at root URL
 app.use('/', express.static('public'));
-
-// router - automatically change routes
-var router = express.Router();
-
-router.get('/todos', function(req, res) {
-	res.json({todos:[]});
-});
-
-// TODO: Add POST route to create new entries
-
-// TODO: Add PUT route to update existing entries
-
-// TODO: Add DELETE route to delete entries
 
 app.use('/api', router);
 
